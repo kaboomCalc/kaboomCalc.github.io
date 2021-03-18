b79118e3e42de579ed7c6cc890aeb26efe89b82f
Kirill Baranov <kirillBaranovJob@yandex.ru>
2021-03-18 12:13:33 +0300

    Add new tab in header (New link to table with price).

---
+++ public/static/1616058739/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fservers\u002Fskyfactory","\u002Fservers\u002Fspacex","\u002Fservers","\u002FResults","\u002Fservers\u002Fnevermine","\u002Fservers\u002Ftesla","\u002Fservers\u002Fdragonglory","\u002Fservers\u002Fedison"]})
